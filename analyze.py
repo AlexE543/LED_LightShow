02a7c15a86a8a989a2f325ee992b6506457b04e0
Alex Ellison <acelli@mit.edu>
2020-03-24 15:54:22 -0500

    trying some filtering

--- analyze.py
+++ analyze.py
@@ -84,8 +84,9 @@ def pulse_to_bars(bars):
 def piano_to_notes(segments):
     strip = neopixel.NeoPixel(board.D18, 144, brightness=0.3, auto_write=True)
     for segment in segments:
-        func.piano(strip, (34, 23, 98), segment["pitches"].index(max(segment["pitches"])))
-        print(segment["pitches"].index(max(segment["pitches"])))
+        if abs(segment["loudness_max"]) - 4 > 0:
+            func.piano(strip, (34, 23, 98), segment["pitches"].index(max(segment["pitches"])))
+            print(segment["pitches"].index(max(segment["pitches"])))
         time.sleep(segment["duration"])
 
 if __name__ == "__main__":
